c4c2b590e1d69866f4e9e5045183247b3e165409
AnnaR11 <ag_01_11@bk.ru>
2021-09-29 12:52:47 +0300

    Задание2. 1-3 пункт.
    создан класс, функция, запускаемый класс

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,5 +5,18 @@ public class MyFirstProgram {
    public static void main(String[] args) {
 
      System.out.println ("Hello, world!");
+     double a=2;
+     double b=3;
+     double c=1;
+     double d=4;
+     System.out.println("Расстояние между двумя точками координат на плоскости" + "=" + (Math.sqrt((a-b)*(a-b)+(c-d)*(c-d))));
+
+     Point p = new Point(5,6,7,8);
+
+
+     System.out.println("Расстояние между двумя точками" + "=" + distance(p));
    }
+  public static double distance (Point p) {
+    return Math.sqrt((p.a - p.b) * (p.a - p.b) + (p.c - p.d) * (p.c - p.d));
+  }
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+  public double a;
+  public double b;
+  public double c;
+  public double d;
+
+  public Point(double a, double b, double c, double d) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.d = d;
+
+  }
+}
